9d3eeefb83fafdeff654ecb210d1d49702e3edbb
Zyna Pen <zynapen@gmail.com>
2021-03-04 11:10:51 -0500

    Removed React Loader and Updated CSS

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <div className="card-body">
         <h1>
           Weather ReactJS
         </h1>
         <Weather />
-      <br />
+        </div>
       <footer className="App-footer">
         This project was coded by <a href="https://www.linkedin.com/in/zynapen/" target="_blank" rel="noreferrer">Zyna Pen</a> and is <a href="https://github.com/zynapen/weather-react" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
       </footer>
+
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
-import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Weather() {
   const [city, setCity] = useState("");
@@ -54,13 +52,6 @@ export default function Weather() {
             <img src={weather.icon} alt={weather.description} />
           </li>
         </ul>
-        <Loader
-        type="Puff"
-        color="#00BFFF"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
       </div>
     );
   } else {
