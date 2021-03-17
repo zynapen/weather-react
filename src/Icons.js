ee02ccc7287248e09830490666ce028a2b4cc2dd
Zyna Pen <zynapen@gmail.com>
2021-03-17 19:08:21 -0400

    Updated CSS

--- src/Icons.js
+++ src/Icons.js
@@ -26,7 +26,7 @@ export default function Icons(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="defaults.color"
+      color="#03506f"
       size={52}
       animate={true}
     />
