97ceedf24dfa9532b87d659b491369138fcc9b88
Zyna Pen <zynapen@gmail.com>
2021-03-18 10:44:39 -0400

    Update CSS and Time

---
+++ src/Time.js
@@ -0,0 +1,64 @@
+export default function Time(){
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let hourFormatted = hours % 12;
+    if (hourFormatted === 0) {
+      hourFormatted = 12;
+    }
+    
+    let minutes = currentTime.getMinutes();
+    let minutesFormatted = currentTime.getMinutes();
+    if (minutes <= 9) {
+      minutesFormatted = `0${minutes}`;
+    }
+    
+    let time = hourFormatted + ":" + minutesFormatted;
+    if (hours >= 12) {
+      time = time + " " + "PM";
+    } else {
+      time = time + " " + "AM";
+    }
+    
+    let dayIndex = currentTime.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    
+    let day = days[dayIndex];
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    
+    let month = months[currentTime.getMonth()];
+    let date = currentTime.getDate();
+    let year = currentTime.getFullYear();
+    
+    let newDate = `${day}, ${month} ${date}, ${year}`;
+    let newTime = `${time}`;
+
+return (
+    <div>
+      {newDate}
+      <br />
+      {newTime}
+      </div>
+  );
+}
+
--- src/WeatherDescription.js
+++ src/WeatherDescription.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Icons from "./Icons";
+import Time from "./Time";
 import Unit from "./Unit";
-import moment from "moment";
 
 export default function WeatherDescription(props) {
   return (
     <div className="WeatherDescription row justify-content-md-center rounded shadow">
-     <div className="col-md-auto"><Icons code={props.data.icon} /><Unit fahrenheit={props.data.temperature} />
+     <div className="col-md-auto">
+      <h1>{props.data.city}</h1>
+          <Time />
+        <p className="text-capitalize">{props.data.description}</p>
+        </div>
+        <div className="col-md-auto"><Icons code={props.data.icon} /><Unit fahrenheit={props.data.temperature} />
      <ul>
             <li>Humidity: {Math.round(props.data.humidity)}%</li>
             <li>Wind: {Math.round(props.data.wind)}mph</li>
           </ul>
      </div>
-     <div className="col-md-auto">
-      <h1>{props.data.city}</h1>
-          {moment().format('dddd')} {moment().format('LT')}
-        <p className="text-capitalize">{props.data.description}</p>
-        </div>
 </div>
   );
 }
