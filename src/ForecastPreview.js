ca67c7da1e6931a64cf467c7723bbd21ec7f21fe
Zyna Pen <zynapen@gmail.com>
2021-03-30 17:23:30 -0400

    Import ForecastPreview CSS

--- src/ForecastPreview.js
+++ src/ForecastPreview.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Icons from "./Icons";
 import moment from "moment";
+import "./ForecastPreview.css";
 
 export default function ForecastPreview(props) {
   function temperature() {
@@ -9,7 +10,7 @@ export default function ForecastPreview(props) {
   }
 
   return (
-    <div className="ForecastPreview col justify-content-md-center m-1 p-2 border-1 rounded shadow">
+    <div className="ForecastPreview col justify-content-md-center m-1 p-2 shadow">
       <Icons code={props.data.weather[0].icon} />
       <br />
       {moment(props.data.dt *1000).format("ddd h A")}
