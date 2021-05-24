cffc066b71303d4035374dfb000ba07ac5347c94
Zyna Pen <zynapen@gmail.com>
2021-05-24 15:20:37 -0400

    Update Footer

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ export default function App() {
   return (
     <div className="container">
       <div className="App">
-        <div className="card-body shadow-lg"><Weather defaultCity="Boston" /></div>
+        <div className="card-body"><Weather defaultCity="Boston" /></div>
       <footer className="App-footer">
-        This project was coded by <a href="https://www.linkedin.com/in/zynapen/" target="_blank" rel="noreferrer">Zyna Pen</a>
+        This project was coded by <a href="https://goofy-dubinsky-84ab58.netlify.app/" target="_blank" rel="noreferrer">Zyna Pen</a>
         <br />
         Open-sourced on <a href="https://github.com/zynapen/weather-react" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://adoring-brown-818a52.netlify.app" target="_blank" rel="noreferrer">Netifly</a>
       </footer>
